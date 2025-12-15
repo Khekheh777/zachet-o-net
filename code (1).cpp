f48bb039f34c681010d259346ff8461a38d2e0ab
khekheh!! <khekheh7@gmail.com>
2025-12-15 15:36:14 +0300

    Add files via upload

---
+++ code (1).cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Employee {
+    private: 
+        string name;
+        int id;
+        double salary;
+    
+    public: 
+        Employee() {
+            name = "";
+            id = 0;
+            salary = 0.0;
+        }
+        Employee(string empName, int empId, double empSalary) {
+            name = empName;
+            id = empId;
+            salary = empSalary;
+        }
+        string getName() const { return name; }
+        int getId() const { return id; }
+        double getSalary() const { return salary; }
+        
+        void setSalary(double newSalary) {
+            if (newSalary >= 0) {
+                salary = newSalary;
+            } else {
+                cout << "ойой чет в минус уходим" << endl;
+            }
+        }
+};
+int main() {
+    const int arrSize = 5;
+    Employee employees[arrSize];
+    for (int i = 0; i <3 ; i++) {
+        cout << "введите данные для работничка #" << (i + 1) << endl;
+        string name;
+        cout << "имя: ";
+        if (i == 0) cin.ignore();
+        getline(cin, name);
+        
+        int id;
+        cout << "айди: ";
+        cin >> id;
+        
+        double salary;
+        cout << "зпшка: ";
+        cin >> salary;
+        
+        employees[i] = Employee(name, id, salary);
+        
+        for (int i = 0; i < 3; i++) {
+            cout << "ID: " << employees[i].getId() << ", Name: " << employees[i].getName() << ", Salary: " << employees[i].getSalary() << endl;
+        }
+    }
+}
